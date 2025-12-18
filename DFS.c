1395b060a65fe2fe90de3f37b5ec51bedaf28b16
ayushinegics24-debug <ayushinegi.cs24@bmsce.ac.in>
2025-12-18 11:53:11 +0530

    Add files via upload

---
+++ DFS.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define MAX 10
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n;
+
+
+void dfs(int v) {
+    int i;
+    visited[v] = 1;
+
+    for (i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int i, j;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (i = 0; i < n; i++) {
+        visited[i] = 0;
+        for (j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    dfs(0);
+
+
+    for (i = 0; i < n; i++) {
+        if (!visited[i]) {
+            printf("Graph is NOT connected\n");
+            return 0;
+        }
+    }
+
+    printf("Graph is CONNECTED\n");
+    return 0;
+}
